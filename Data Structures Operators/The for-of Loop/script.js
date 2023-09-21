9b0f5a2114057f3b1cde3a284641bda16bae18a1
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-09-21 18:36:38 +0300

    Create script.js

---
+++ Data Structures Operators/The for-of Loop/script.js
@@ -0,0 +1,9 @@
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+
+// console.log([...menu.entries()]);
