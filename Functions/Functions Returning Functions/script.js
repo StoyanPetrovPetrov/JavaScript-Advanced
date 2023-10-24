ad0976e36d89b3dbc6be71e3f8b320dafb7ca40b
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-10-24 17:56:05 +0300

    Create script.js

---
+++ Functions/Functions Returning Functions/script.js
@@ -0,0 +1,16 @@
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+// Challenge
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArr('Hi')('Jonas');
